afa72fb38559362d6d565231449e8ab4202b3b16
Foong-Ann <54426916+Foong-Ann@users.noreply.github.com>
2020-04-20 02:19:22 +0800

    Update main.py

--- main.py
+++ main.py
@@ -5,11 +5,11 @@ import signal
 
 os.chdir(os.getcwd()+'/jpm_module_1')
 
-process = subprocess.Popen(['python', 'server3.py'], cwd=os.getcwd(), preexec_fn=os.setsid)
+process = subprocess.Popen(['python', 'server.py'], cwd=os.getcwd(), preexec_fn=os.setsid)
 
 time.sleep(.300)
 
-process2 = subprocess.Popen(['python', 'client3.py'], cwd=os.getcwd(), preexec_fn=os.setsid)
+process2 = subprocess.Popen(['python', 'client.py'], cwd=os.getcwd(), preexec_fn=os.setsid)
 process2.wait()
 os.killpg(os.getpgid(process.pid), signal.SIGTERM)
 
