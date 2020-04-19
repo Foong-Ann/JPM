1b7f0deb96d2c0e6f6759484dcf4591475d7f931
Foong-Ann <54426916+Foong-Ann@users.noreply.github.com>
2020-04-20 02:55:22 +0800

    Add more unit tests for getRatio

--- jpm_module_1/client_test.py
+++ jpm_module_1/client_test.py
@@ -1,6 +1,7 @@
 import unittest
-from client import getDataPoint
+from client import *
 
+##test getDataPoint
 class ClientTest(unittest.TestCase):
   def test_getDataPoint_calculatePrice(self):
     quotes = [
@@ -17,8 +18,27 @@ class ClientTest(unittest.TestCase):
     ]
     for quote in quotes:
       self.assertEqual(getDataPoint(quote),(quote['stock'], quote['top_bid']['price'],quote['top_ask']['price'], (quote['top_bid']['price']+quote['top_ask']['price'])/2))
- 
- """ ------------ Add more unit tests ------------ """
+
+##test getRatio
+  def test_getRatio_price_b_notZero(self):
+    quotes = [
+      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    price_a= (quotes[0]['top_bid']['price']+quotes[0]['top_ask']['price'])/2
+    price_b= (quotes[1]['top_bid']['price']+quotes[1]['top_ask']['price'])/2
+    self.assertEqual(getRatio(price_a,price_b), price_a/price_b)
+
+  def test_getRatio_price_b_isZero(self):
+    quotes = [
+      {'top_ask': {'price': 121.2, 'size': 36}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 120.48, 'size': 109}, 'id': '0.109974697771', 'stock': 'ABC'},
+      {'top_ask': {'price': 0, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 0, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
+    ]
+    price_a= (quotes[0]['top_bid']['price']+quotes[0]['top_ask']['price'])/2
+    price_b= (quotes[1]['top_bid']['price']+quotes[1]['top_ask']['price'])/2
+    if (price_b==0):
+      return
+    self.assertEqual(getRatio(price_a,price_b), price_a/price_b)
 
 
 if __name__ == '__main__':
